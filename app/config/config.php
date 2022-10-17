2508740d94ef9a3a10d1657e9a02559177f03ac7
fdschepper <333476@student.mboutrecht.nl>
2022-10-17 11:47:59 +0200

    init?

--- app/config/config.php
+++ app/config/config.php
@@ -2,10 +2,10 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvcframework');
+define('DB_NAME', 'mvc-oop-toets');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 
-define('URLROOT', 'http://mvctoetsbackend/');
+define('URLROOT', 'http://mvc-oop-toets.com');
 
 define('SITENAME', 'MVC Framework reference');
