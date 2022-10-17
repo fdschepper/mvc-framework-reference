eb4eef634ec9907ce3210cd23b038e6553526e66
fdschepper <333476@student.mboutrecht.nl>
2022-10-17 10:55:16 +0200

    initial commit

--- app/config/config.php
+++ app/config/config.php
@@ -4,9 +4,8 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'mvcframework');
 
-define('APPROOT', dirname(dirname(__FILE__))) ;
+define('APPROOT', dirname(dirname(__FILE__)));
 
-define('URLROOT', 'http://mvc-framework-reference.org');
+define('URLROOT', 'http://mvctoetsbackend/');
 
 define('SITENAME', 'MVC Framework reference');
-?>
